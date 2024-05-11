ae1ff8903f74696a459004f4dfb49584d5f1996b
jsKim <ewhdtls0@naver.com>
2024-05-11 19:10:53 +0900

    섹션1 - HelloWorld Bean 추가

--- src/main/java/study/myrestfulservice/controller/HelloWorldController.java
+++ src/main/java/study/myrestfulservice/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package study.myrestfulservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import study.myrestfulservice.bean.HelloWorldBean;
 
 @RestController
 public class HelloWorldController {
@@ -9,5 +10,10 @@ public class HelloWorldController {
     @GetMapping("/hello-world")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
     }
 }
