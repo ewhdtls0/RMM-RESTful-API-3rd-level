ae7ebdbb7f11f284697b92af88f1181d1c9eac6b
jsKim <ewhdtls0@naver.com>
2024-05-13 16:19:15 +0900

    섹션5: JPA Entity 설정

--- src/main/java/study/myrestfulservice/config/SecurityConfig.java
+++ src/main/java/study/myrestfulservice/config/SecurityConfig.java
@@ -1,33 +1,33 @@
-package study.myrestfulservice.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-@Configuration
-public class SecurityConfig {
-
-    @Bean
-    UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
-
-        UserDetails newUser = User.withUsername("user")
-                .password(passwordEncoder().encode("passw0rd"))
-                .authorities("read")
-                .build();
-
-        userDetailsManager.createUser(newUser);
-
-        return userDetailsManager;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-}
+//package study.myrestfulservice.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//
+//@Configuration
+//public class SecurityConfig {
+//
+//    @Bean
+//    UserDetailsService userDetailsService() {
+//        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+//
+//        UserDetails newUser = User.withUsername("user")
+//                .password(passwordEncoder().encode("passw0rd"))
+//                .authorities("read")
+//                .build();
+//
+//        userDetailsManager.createUser(newUser);
+//
+//        return userDetailsManager;
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//}
