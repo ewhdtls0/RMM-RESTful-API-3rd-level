191ac70833e9896c276e402a9183f1fb6caf23b4
jsKim <ewhdtls0@naver.com>
2024-05-13 16:44:25 +0900

    섹션5: JPA 유저 상세

--- src/main/java/study/myrestfulservice/controller/UserJpaController.java
+++ src/main/java/study/myrestfulservice/controller/UserJpaController.java
@@ -1,14 +1,16 @@
 package study.myrestfulservice.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import study.myrestfulservice.bean.User;
+import study.myrestfulservice.exception.UserNotFoundException;
 import study.myrestfulservice.repository.UserRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/jpa")
@@ -20,5 +22,21 @@ public class UserJpaController {
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<EntityModel<User>> retrieveUser(@PathVariable int id) {
+        Optional<User> user = userRepository.findById(id);
+
+        if(!user.isPresent()) {
+            throw new UserNotFoundException("id-" + id);
+        }
+
+        EntityModel<User> entityModel = EntityModel.of(user.get());
+        WebMvcLinkBuilder linkTo = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(linkTo.withRel("all-users"));
+
+        return ResponseEntity.ok(entityModel);
+
     }
 }
