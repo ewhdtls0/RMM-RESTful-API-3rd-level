e6fb3fc8678c0f43aac90c396c90887eeb22cd54
jsKim <ewhdtls0@naver.com>
2024-05-13 16:28:35 +0900

    섹션5: JPA 초기 데이터 설정, 시큐리티 설정

--- src/main/java/study/myrestfulservice/config/SecurityConfig.java
+++ src/main/java/study/myrestfulservice/config/SecurityConfig.java
@@ -1,33 +1,41 @@
-//package study.myrestfulservice.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//
-//@Configuration
-//public class SecurityConfig {
-//
-//    @Bean
-//    UserDetailsService userDetailsService() {
-//        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
-//
-//        UserDetails newUser = User.withUsername("user")
-//                .password(passwordEncoder().encode("passw0rd"))
-//                .authorities("read")
-//                .build();
-//
-//        userDetailsManager.createUser(newUser);
-//
-//        return userDetailsManager;
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//}
+package study.myrestfulservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+
+        UserDetails newUser = User.withUsername("user")
+                .password(passwordEncoder().encode("passw0rd"))
+                .authorities("read")
+                .build();
+
+        userDetailsManager.createUser(newUser);
+
+        return userDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+    }
+}
