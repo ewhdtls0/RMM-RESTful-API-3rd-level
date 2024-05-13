457d1181c5d83c1980dac2280cb7661d6e4db7bb
jsKim <ewhdtls0@naver.com>
2024-05-13 17:05:59 +0900

    섹션5: Post Entity 추가 및 User 연관관계 설정

---
+++ src/main/java/study/myrestfulservice/bean/Post.java
@@ -0,0 +1,24 @@
+package study.myrestfulservice.bean;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
+}
